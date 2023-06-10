7a9b829871bb37ea6f78a10644591f881973465d
Vicente Debrun <debrunvicente@gmail.com>
2023-06-09 20:07:01 -0700

    refactor viewsets

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from app.models import User, Category, Expenses
+from app.serializers import UserSerializer, CategorySerializer, ExpensesSerializer
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ExpensesViewSet(viewsets.ModelViewSet):
+    queryset = Expenses.objects.all()
+    serializer_class = ExpensesSerializer
