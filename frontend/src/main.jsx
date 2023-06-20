90fee754dc074250549717c74a1255c00264fd75
Vicente Debrun <debrunvicente@gmail.com>
2023-06-20 11:47:02 -0700

    refact router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App.jsx'
-import './index.css'
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+import { BrowserRouter } from "react-router-dom";
 
-root.render(<App />)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
