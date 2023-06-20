1b634b771aa30d88137848b41f1fd6f7ff4f3a9f
Vicente Debrun <debrunvicente@gmail.com>
2023-06-20 11:46:31 -0700

    refact react struct router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,27 @@
-import { Router } from './components/Router.jsx';
+import { Login } from "./views/Login";
+import { Register } from "./views/Register";
+import { Dashboard } from "./views/Dashboard";
+import { Routes, Route } from "react-router-dom";
+
+import { createContext, useState } from "react";
+
+export const UserContext = createContext(null);
 
 export function App() {
-    return (
-        <Router />
-    );
+  const [userData, setUserData] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <UserContext.Provider
+      value={{ userData, setUserData, isLoggedIn, setIsLoggedIn }}
+    >
+      <Routes>
+        <Route index element={<div>Landing</div>} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="*" element={<div>Error 404</div>} />
+      </Routes>
+    </UserContext.Provider>
+  );
 }
